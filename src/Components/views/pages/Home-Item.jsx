2c5633b99fcbfd22df0c7f5e4c8a8e1dd0ded82c
selviaeld <selviaeldina12@gmail.com>
2022-03-22 22:21:05 +0700

    styling UI

--- src/Components/views/pages/Home-Item.jsx
+++ src/Components/views/pages/Home-Item.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Home from "./Home";
 import data from "../../data/DataDummy";
-import "./Home.css";
+import "../styles/Home.css";
 
 function HomeItem() {
     return (
-      <div className="card-item">
+      <div className="home-item">
         <Home title={data.name} artist={data.artists[0].name} album={data.album.name} />
       </div>
     );
