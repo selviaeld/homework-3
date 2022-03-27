7a2d25fff56c47fdc84b047b01851f0c2e2e3a23
selviaeld <selviaeldina12@gmail.com>
2022-03-27 22:31:32 +0700

    mapping

--- src/Pages/Home-Item.jsx
+++ src/Pages/Home-Item.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Home from "../Components/Home/";
-import data from "../Components/data/DataDummy";
+import Data from "../Components/data/DataDummy";
 import "../Components/Home/Home.css";
 
 function HomeItem() {
   return (
     <div className="home-item">
-      <Home image={data.album.images[0].url} title={data.name} artist={data.artists[0].name} album={data.album.name} />
+      {
+        Data.map(D => (
+          <Home key={D.id} image={D.album.images[0].url} title={D.name} artist={D.artists[0].name} album={D.album.name}/>
+        ))
+      }
     </div>
   );
 }
